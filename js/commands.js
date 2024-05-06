97ac5369f8cc6f60de19f78ba6fd09fb20b4872b
kenngineer <162806245+kenngineer@users.noreply.github.com>
2024-05-06 13:44:48 +0800

    portfolio.beta

---
+++ js/commands.js
@@ -0,0 +1,111 @@
+var youtube = "https://www.youtube.com/@kenngineerofficial/";
+var linkedin = "https://www.linkedin.com/in/kenngineer/";
+var instagram = "https://www.instagram.com/kenngineer/";
+var github = "https://github.com/kenngineer/";
+var email = 'mailto:kenngineer@outlook.com';
+var facebook = 'https://www.facebook.com/kenngineering/';
+
+about = [
+  "<br>",
+  "Hey there, I'm Kenny!👋",
+  "I'm an electrical engineer by profession and now I am making the change in my career to pursue Cybersecurity.",
+  "I am also an aspiring YouTuber and provide cybersecurity-niched contents for my audience;",
+  "that helps me learn more as a teach what i know.",
+  "That is my way of documenting my growth as a Cybersecurity professional. ",
+  "I like to tinker around with computers. Building DIY home servers, NASes, and setting up Labs @home.",
+  "@kenngineer prefers Linux as a daily driver, because why not? it's cleaner, it's lightweight, it's open-source, and no bloatware.",
+  "My dream is to run my very own cybersecurity agency. from government contracts to corporate giants; both",
+  "for red team and blue team services. Too big of a dream? maybe. but it's not impossible",
+  "  ",
+  "  ",
+  "@kennginer's ARSENAL:",
+  "PYTHON     : [████████████--------] 60%",
+  "NETWORKING : [████████-----------] 40%",
+  "LINUX      : [██████████████------] 70%",
+  "TCPDUMP    : [███████████--------] 55%",
+  "WIRESHARK  : [██████████---------] 50%",
+  "SPLUNK     : [█████--------------] 25%",
+  "CLOUD      : [███-----------------] 15%",
+  "SQL        : [████████████--------] 60%",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+
+  "<br>"
+];
+
+
+
+social = [
+  "<br>",
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/kenngineerofficial' + "</a>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/kenngineer' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/kenngineer' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/kenngineer' + "</a>",
+  'facebook       <a href="' + facebook + '" target="_blank">facebook/kenngineering' + "</a>",
+  "<img src='img/rick.gif' style='width: 400px; height: auto; margin-left: 100px;'>",  
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">about</span>          who is @kenngineer?',
+  '<span class="command">video</span>          view YouTube videos',
+  '<span class="command">social</span>         display social networks',
+  '<span class="command">projects</span>       view projects',
+  '<span class="command">history</span>        view command history',
+  '<span class="command">email</span>          email me',
+  '<span class="command">clear</span>          clear terminal',
+  '<span class="command">banner</span>         display the header',
+  "<br>",
+];
+
+banner = [
+  "oooo                                                      o8o                                           o8o",
+  "`888                                                      `\"'                                           `YP",
+  " 888  oooo   .ooooo.  ooo. .oo.   ooo. .oo.    .oooooooo oooo  ooo. .oo.    .ooooo.   .ooooo.  oooo d8b  '   .oooo.o",
+  " 888 .8P'   d88' `88b `888P\"Y88b  `888P\"Y88b  888' `88b  `888  `888P\"Y88b  d88' `88b d88' `88b `888\"\"8P     d88(  \"8",
+  " 888888.    888ooo888  888   888   888   888  888   888   888   888   888  888ooo888 888ooo888  888         `\"Y88b.",
+  " 888 `88b.  888    .o  888   888   888   888  `88bod8P'   888   888   888  888    .o 888    .o  888         o.  )88b",
+  "o888o o888o `Y8bod8P' o888o o888o o888o o888o `8oooooo.  o888o o888o o888o `Y8bod8P' `Y8bod8P' d888b        8\"\"888P'",
+  "                                              d\"     YD",
+  "                                              \"Y88888P'",
+  "",
+  "                                  .    .o88o.           oooo   o8o",
+  "                                .o8    888 `\"           `888   `\"'",
+  "oo.ooooo.   .ooooo.  oooo d8b .o888oo o888oo   .ooooo.   888  oooo   .ooooo.",
+  " 888' `88b d88' `88b `888\"\"8P   888    888    d88' `88b  888  `888  d88' `88b",
+  " 888   888 888   888  888       888    888    888   888  888   888  888   888",
+  " 888   888 888   888  888       888 .  888    888   888  888   888  888   888 ",
+  " 888bod8P' `Y8bod8P' d888b      \"888\" o888o   `Y8bod8P' o888o o888o `Y8bod8P'   © 2024",
+  " 888                                                           credits to @ForrestKnight for the template :)",
+  "o888o",
+  "   ",
+  
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
